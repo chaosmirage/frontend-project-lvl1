dc8530161de7f887c9dfa9b9c610ca8d5124071c
Adel Khamatov <adelkhamatov@yandex.ru>
2021-02-22 20:22:04 +0300

    Refactor

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+import readlineSync from "readline-sync";
 import run from "../src/cli.js";
 
-run();
+try {
+  console.log("Welcome to the Brain Games!");
+  const name = readlineSync.question("May I have your name? ");
+  run({ name });
+} catch (e) {
+  console.log(e);
+}
--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,5 @@
-import readlineSync from "readline-sync";
-
-export default () => {
-  try {
-    console.log("Welcome to the Brain Games!");
-    const name = readlineSync.question("May I have your name? ");
-    if (name) {
-      console.log(`Hello, ${name}`);
-    }
-  } catch (e) {
-    console.error(e);
+export default ({ name }) => {
+  if (name) {
+    console.log(`Hello, ${name}`);
   }
 };
