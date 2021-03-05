08a12c17ff6e4f69b5740e3f076431494975a230
Adel Khamatov <adelkhamatov@yandex.ru>
2021-03-05 17:30:23 +0300

    Fix: change brain-even bin

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-import cli from '../src/cli.js';
+import run from '../src/games/brain-even.js';
 
-try {
-  cli('even');
-} catch (error) {
-  console.log(error);
-}
+run();
--- src/cli.js
+++ src/cli.js
@@ -1,23 +1,6 @@
-import runCalc from './games/brain-calc.js';
-import runEven from './games/brain-even.js';
-import runGames from './games/brain-games.js';
-import runGCD from './games/brain-gcd.js';
-import runProgression from './games/brain-progression.js';
-import runPrime from './games/brain-prime.js';
+import readlineSync from 'readline-sync';
 
-export default (gameName) => {
-  const run = {
-    calc: runCalc,
-    even: runEven,
-    games: runGames,
-    gcd: runGCD,
-    progression: runProgression,
-    prime: runPrime,
-  }[gameName];
-
-  if (!run) {
-    throw new Error(`Unexpected game: ${gameName}`);
-  }
-
-  run();
+export default () => {
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
 };
